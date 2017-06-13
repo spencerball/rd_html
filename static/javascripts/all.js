a30080c7ece69af988dcf75d63292493c1b41d0b
u58018 <u58018@f5bd8f7e-1898-43c9-955e-49874a9ecb43>
2017-06-13 17:32:09 +0000

    Static site pushed with build timestamp 20170613_173205.319996

--- static/javascripts/all.js
+++ static/javascripts/all.js
@@ -191,7 +191,9 @@ function Table(table) {
 
   var module = this;
   var $table = table ?  table : $("#table");
-  var $headings = $table.find('thead td'), ordering, cachedIndex;
+  var groupLength = $table.find('thead tr').first().find('td').length - 1;
+  var cellLength = $table.find('thead tr td').length;
+  var $headings = $table.find('thead tr').last().find('td'), ordering, cachedIndex;
 
   this.ordering = function(index) {
     var firstClick = cachedIndex !== index;
@@ -204,12 +206,37 @@ function Table(table) {
   }
 
   if($headings.length) {
-
     var dataTable = $table.DataTable({
       "paging":   false,
       "searching": false,
       "info":     false,
     });
+
+    function createGroupedTables() {
+      var $categories = $table.find('thead tr').first().children();
+      var $labels = $table.find('thead tr').last().children();
+      $.each($table.find('tbody tr'), function () {
+        var $cells = $(this).find('td');
+        var x = $cells.length / ($categories.length - 1);
+        var lineIndexes = [];
+        
+        // create array containing indexes of tables cell requiring a dividing line
+        for (var i = 1; i <= x; i++) {
+          lineIndexes.push((x * i) - 1);
+        }
+
+        // pop last array item so that a line isn't added to right edge of table
+        if (lineIndexes.length > 1) {
+          lineIndexes.pop();
+        }
+
+        // add class to table cells numbers from lineIndex array
+        for (var i = 0; i < lineIndexes.length; i++) {
+          $($cells[lineIndexes[i]]).addClass('line');
+          $($labels[lineIndexes[i] + 1]).addClass('line');
+        }
+      });
+    }
 
     $.each($headings, function (index) {
       var $button = $(this).find('button');
@@ -217,12 +244,17 @@ function Table(table) {
         module.ordering(index);
         $(this).unbind().attr('class', 'sorting_' + ordering);
         dataTable.order( [index,  ordering]).draw()
+        createGroupedTables();
       }.bind(this))
     });
 
     $headings.attr('width', (960 / $headings.length));
     $headings.removeAttr('style');
     $table.removeAttr('style');
+
+    if($table.hasClass('grouped')) {
+      createGroupedTables();
+    }
   }
 
   return module;
