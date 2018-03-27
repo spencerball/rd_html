0138cba89ec8775b83dd66f8287aaee196626998
u53264 <u53264@4a104435-3b38-4549-9215-19f95169377e>
2018-03-27 11:36:11 +0000

    Static site pushed with build timestamp 20180327_112427.029316

--- static/javascripts/all.js
+++ static/javascripts/all.js
@@ -920,6 +920,30 @@ if (
   })
 
 }
+
+function showDeleteForm(measure) {
+    document.getElementById('measure-action-section-' + measure).style.display = 'none';
+    document.getElementById('measure-action-section__delete-' + measure).style.display = 'block';
+    return false;
+}
+
+function submitDeleteForm(measure) {
+    var yesBtn = document.getElementById('delete-radio-yes-' + measure);
+    var noBtn = document.getElementById('delete-radio-no-' + measure);
+    if(yesBtn.checked === true) {
+        console.log("true");
+        return true;
+    } else {
+        console.log("false or other");
+        return hideDeleteForm(measure);
+    }
+}
+
+function hideDeleteForm(measure) {
+    document.getElementById('measure-action-section__delete-' + measure).style.display = 'none';
+    document.getElementById('measure-action-section-' + measure).style.display = 'block';
+    return false;
+}
 document.addEventListener('DOMContentLoaded', function() {
 
   var detailsElements = document.getElementsByTagName('DETAILS');
