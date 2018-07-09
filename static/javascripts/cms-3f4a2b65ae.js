7636df40185382771f21ec672a819cf7d7689d48
u54319 <u54319@e2de6956-82bf-4ef4-838b-250e3d87b9c3>
2018-07-09 11:21:22 +0000

    Static site pushed with build timestamp 20180709_111628.645180

--- static/javascripts/cms-3f4a2b65ae.js
+++ static/javascripts/cms-3f4a2b65ae.js
@@ -298,12 +298,12 @@ function ukCountriesSelect(element) {
 /**
  * Created by Tom.Ridd on 25/02/2018.
 
-rd-builder provides common functions that are required by both the chartbuilder and tablebuilder screens
+ rd-builder provides common functions that are required by both the chartbuilder and tablebuilder screens
 
-specifically
-- validate that data does not have multiple rows that correspond to a single data point
-- validate that data has coverage of every data point
-- provide useful error messages when data is invalid
+ specifically
+ - validate that data does not have multiple rows that correspond to a single data point
+ - validate that data has coverage of every data point
+ - provide useful error messages when data is invalid
 
  */
 
@@ -311,8 +311,7 @@ specifically
 var DATA_ERROR_DUPLICATION = "duplication";
 var DATA_ERROR_MISSING_DATA = "missing data";
 var DATA_ERROR_SETTINGS_ERROR = "settings";
-
-
+var DATA_ERROR_COMPLEX_DATA = "complex data";
 
 
 // ---------------------------------------------------------------------------
@@ -324,16 +323,26 @@ function validateData(data, categoryColumn, groupColumn) {
     var errors = [];
     var dataRows = _.clone(data);
     var headerRow = dataRows.shift();
-    var lowerHeaderRow = _.map(headerRow, function(m) { return m.trim().toLowerCase()})
+    var lowerHeaderRow = _.map(headerRow, function (m) {
+        return m.trim().toLowerCase()
+    })
 
     var categoryIndex = index_of_column_named(lowerHeaderRow, categoryColumn);
-    if(categoryIndex === null) {
-        return [{'error': 'could not find data column', 'column': categoryColumn, 'errorType': DATA_ERROR_SETTINGS_ERROR}]
+    if (categoryIndex === null) {
+        return [{
+            'error': 'could not find data column',
+            'column': categoryColumn,
+            'errorType': DATA_ERROR_SETTINGS_ERROR
+        }]
     }
-    if(groupColumn !== null) {
+    if (groupColumn !== null) {
         var groupIndex = index_of_column_named(lowerHeaderRow, groupColumn);
-        if(groupIndex === null) {
-            return [{'error': 'could not find data column', 'column': groupColumn, 'errorType': DATA_ERROR_SETTINGS_ERROR}]
+        if (groupIndex === null) {
+            return [{
+                'error': 'could not find data column',
+                'column': groupColumn,
+                'errorType': DATA_ERROR_SETTINGS_ERROR
+            }]
         } else {
             return validateGroupedData(dataRows, categoryIndex, groupIndex, categoryColumn, groupColumn)
         }
@@ -346,16 +355,18 @@ function validateDataDuplicatesOnly(data, categoryColumn, groupColumn) {
     var errors = [];
     var dataRows = _.clone(data);
     var headerRow = dataRows.shift();
-    var lowerHeaderRow = _.map(headerRow, function(m) { return m.trim().toLowerCase(); });
+    var lowerHeaderRow = _.map(headerRow, function (m) {
+        return m.trim().toLowerCase();
+    });
     var categoryIndex = index_of_column_named(lowerHeaderRow, categoryColumn);
 
-    if(categoryIndex === null) {
+    if (categoryIndex === null) {
         return [{'error': 'could not find data column', 'column': categoryColumn}]
     }
-    if(groupColumn !== null) {
+    if (groupColumn !== null) {
         var groupIndex = index_of_column_named(lowerHeaderRow, groupColumn);
 
-        if(groupIndex === null) {
+        if (groupIndex === null) {
             return [{'error': 'could not find data column', 'column': groupColumn}]
         } else {
             return validateGroupedDataDuplicates(dataRows, categoryIndex, groupIndex)
@@ -366,9 +377,6 @@ function validateDataDuplicatesOnly(data, categoryColumn, groupColumn) {
 }
 
 
-
-
-
 // ---------------------------------------------------------------------------
 // SIMPLE DATA (not cross tab, multiseries, panel, etc...)
 // ---------------------------------------------------------------------------
@@ -376,30 +384,38 @@ function validateDataDuplicatesOnly(data, categoryColumn, groupColumn) {
 function validateSimpleData(data, categoryIndex, categoryColumn) {
     var duplicateErrors = [];
 
-    var dict = {};
-    _.forEach(data, function (row) {
-       var value = row[categoryIndex];
-        if(value in dict){
-            // wrap in if to make sure we don't add multiple error messages
-           if(dict[value] !== 'added to errors') {
-               duplicateErrors.push({
-                   'error': 'duplicate data',
-                   'category': value,
-                   'categoryColumn': categoryColumn,
-                   'errorType': DATA_ERROR_DUPLICATION
-               });
-               dict[value] = 'added to errors'
-           }
-        } else {
-           dict[value] = 'value in dict'
-        }
-    });
+    var uniqueCategories = _.uniq(_.map(data, function (row) {
+        return row[categoryIndex];
+    }));
 
-    return duplicateErrors;
+    if (uniqueCategories.length === data.length) {
+        return [];
+    } else if (data.length % uniqueCategories.length === 0) {
+        return [{'errorType': DATA_ERROR_COMPLEX_DATA}]
+    } else {
+        var dict = {};
+        _.forEach(data, function (row) {
+            var value = row[categoryIndex];
+            if (value in dict) {
+                // wrap in if to make sure we don't add multiple error messages
+                if (dict[value] !== 'added to errors') {
+                    duplicateErrors.push({
+                        'error': 'duplicate data',
+                        'category': value,
+                        'categoryColumn': categoryColumn,
+                        'errorType': DATA_ERROR_DUPLICATION
+                    });
+                    dict[value] = 'added to errors'
+                }
+            } else {
+                dict[value] = 'value in dict'
+            }
+        });
+
+        return duplicateErrors;
+    }
+
 }
-
-
-
 
 
 // ---------------------------------------------------------------------------
@@ -414,20 +430,33 @@ function validateGroupedData(data, categoryIndex, groupIndex, categoryColumn, gr
 }
 
 function validateGroupedDataCompleteness(data, categoryIndex, groupIndex, categoryColumn, groupColumn) {
-    var rowItems = _.uniq(_.map(data, function(item) { return item[categoryIndex]; }));
-    var columnItems = _.uniq(_.map(data, function(item) { return item[groupIndex]; }));
+    var rowItems = _.uniq(_.map(data, function (item) {
+        return item[categoryIndex];
+    }));
+    var columnItems = _.uniq(_.map(data, function (item) {
+        return item[groupIndex];
+    }));
     var errors = [];
 
-    var mapOfPairs = _.object(_.map(rowItems, function(item) {
-       return [item, _.map(_.filter(data, function(row) { return row[categoryIndex] === item}), function (row) {
+    var mapOfPairs = _.object(_.map(rowItems, function (item) {
+        return [item, _.map(_.filter(data, function (row) {
+            return row[categoryIndex] === item
+        }), function (row) {
             return row[groupIndex];
-       })];
+        })];
     }));
 
     _.forEach(rowItems, function (row) {
         _.forEach(columnItems, function (col) {
-            if(!_.contains(mapOfPairs[row], col)) {
-                errors.push({'error':'missing data', 'category': row, 'group': col, 'categoryColumn': categoryColumn, 'groupColumn': groupColumn, 'errorType': DATA_ERROR_MISSING_DATA})
+            if (!_.contains(mapOfPairs[row], col)) {
+                errors.push({
+                    'error': 'missing data',
+                    'category': row,
+                    'group': col,
+                    'categoryColumn': categoryColumn,
+                    'groupColumn': groupColumn,
+                    'errorType': DATA_ERROR_MISSING_DATA
+                })
             }
         })
     });
@@ -442,9 +471,16 @@ function validateGroupedDataDuplicates(data, categoryIndex, groupIndex, category
     _.forEach(data, function (row) {
         var categoryValue = row[categoryIndex];
         var groupValue = row[groupIndex];
-        if(categoryValue in groupValuesUsedByCategory) {
-            if(groupValue in groupValuesUsedByCategory[categoryValue]) {
-                errors.push({'error':'duplicate data', 'category': row[categoryIndex], 'group':row[groupIndex], 'categoryColumn': categoryColumn, 'groupColumn': groupColumn, 'errorType': DATA_ERROR_DUPLICATION})
+        if (categoryValue in groupValuesUsedByCategory) {
+            if (groupValue in groupValuesUsedByCategory[categoryValue]) {
+                errors.push({
+                    'error': 'duplicate data',
+                    'category': row[categoryIndex],
+                    'group': row[groupIndex],
+                    'categoryColumn': categoryColumn,
+                    'groupColumn': groupColumn,
+                    'errorType': DATA_ERROR_DUPLICATION
+                })
             } else {
                 groupValuesUsedByCategory[categoryValue][groupValue] = 1;
             }
@@ -458,52 +494,44 @@ function validateGroupedDataDuplicates(data, categoryIndex, groupIndex, category
 }
 
 
-
-
-
 // ---------------------------------------------------------------------------
 // ERROR REPORTING
 // ---------------------------------------------------------------------------
 
 function errorDescription(error) {
     switch (error.errorType) {
-      case DATA_ERROR_SETTINGS_ERROR:
-        return "Column '" + error.column + "' not found";
+        case DATA_ERROR_SETTINGS_ERROR:
+            return "Column '" + error.column + "' not found";
 
-      case DATA_ERROR_MISSING_DATA:
-        return "The data is missing a row for " + error.categoryColumn + " = '" + error.category + "' and " + error.groupColumn + " = '" + error.group + "'"
+        case DATA_ERROR_MISSING_DATA:
+            return "The data is missing a row for " + error.categoryColumn + " = '" + error.category + "' and " + error.groupColumn + " = '" + error.group + "'"
 
-      case DATA_ERROR_DUPLICATION:
-        if('group' in error) {
-            return "The data has duplicate entries for the rows with " + error.categoryColumn + " = '" + error.category + "' and " + error.groupColumn + " = '" + error.group + "'"
-        } else {
-            return "The data has duplicate entries for " + error.categoryColumn + " = '" + error.category + "'"
-        }
+        case DATA_ERROR_DUPLICATION:
+            if ('group' in error) {
+                return "The data has duplicate entries for the rows with " + error.categoryColumn + " = '" + error.category + "' and " + error.groupColumn + " = '" + error.group + "'"
+            } else {
+                return "The data has duplicate entries for " + error.categoryColumn + " = '" + error.category + "'"
+            }
     }
 }
 
 function errorResolutionHint(error) {
     switch (error.errorType) {
-      case DATA_ERROR_SETTINGS_ERROR:
-        return "Make sure the column values selected for this table are valid";
+        case DATA_ERROR_SETTINGS_ERROR:
+            return "Make sure the column values selected for this table are valid";
 
-      case DATA_ERROR_MISSING_DATA:
-        return "Add rows to your source spreadsheet and try again";
+        case DATA_ERROR_MISSING_DATA:
+            return "Add rows to your source spreadsheet and try again";
 
-      case DATA_ERROR_DUPLICATION:
-        return "Remove data rows in your source spreadsheet and try again"
+        case DATA_ERROR_DUPLICATION:
+            return "Remove data rows in your source spreadsheet and try again"
 
     }
 }
 
 
-
-
-
-
-
 // If we're running under Node - required for testing
-if(typeof exports !== 'undefined') {
+if (typeof exports !== 'undefined') {
     var _ = require('../charts/vendor/underscore-min');
     var dataTools = require('../charts/rd-data-tools');
     var index_of_column_named = dataTools.index_of_column_named;
@@ -513,6 +541,7 @@ if(typeof exports !== 'undefined') {
     exports.validateData = validateData;
     exports.DATA_ERROR_DUPLICATION = DATA_ERROR_DUPLICATION;
     exports.DATA_ERROR_MISSING_DATA = DATA_ERROR_MISSING_DATA;
+    exports.DATA_ERROR_COMPLEX_DATA = DATA_ERROR_COMPLEX_DATA;
     exports.DATA_ERROR_SETTINGS_ERROR = DATA_ERROR_SETTINGS_ERROR;
 }
 
@@ -541,11 +570,11 @@ THE PARENT COLOUR SECTION IS TO DO WITH DISPLAY, NOT DATA CONTENT
 var defaultParentColor = '#2B8CC4';
 var defaultChildColor = '#B3CBD9';
 var VERSION = '1.1'; // panel charts include sort option
-var BAR_CHART = 'bar'
-var LINE_CHART = 'line'
-var COMPONENT_CHART = 'component'
-var PANEL_BAR_CHART = 'panel_bar'
-var PANEL_LINE_CHART = 'panel_line'
+var BAR_CHART = 'bar';
+var LINE_CHART = 'line';
+var COMPONENT_CHART = 'component';
+var PANEL_BAR_CHART = 'panel_bar';
+var PANEL_LINE_CHART = 'panel_line';
 
 
 
